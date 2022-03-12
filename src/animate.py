c8ed45fd20a45addbb59fcf5435a68418da827cc
crayjake <mm18j2c@leeds.ac.uk>
2022-03-12 15:40:21 +0000

    initial commit

---
+++ src/animate.py
@@ -0,0 +1,126 @@
+## Generation of plots/videos/gifs based off of .npy files
+import numpy as np
+from tqdm import tqdm
+import os
+import math
+
+import matplotlib
+#matplotlib.use('Agg')
+from matplotlib import pyplot as plt
+
+import matplotlib as mpl
+mpl.rcParams['figure.dpi'] = 300
+
+from matplotlib import colors
+
+from data import Data
+
+import subprocess
+
+from enum import Enum
+
+class Plot(Enum):
+    STREAM=0,
+    LINE=1
+
+class LineType(Enum):
+    U=1,
+    W=2,
+    B=3,
+    P=4,
+    V=5,
+    ALL=0
+
+class animator():
+
+    def images(self, data: Data, path, title:bool = False, plotType:Plot = Plot.STREAM, lineType:LineType = None):
+        skip = int(data.meta.x.shape[1]/200)
+        prefix = ''
+        if plotType == Plot.STREAM:
+            prefix = 'stream'
+        elif plotType == Plot.LINE:
+            prefix = 'line'
+
+        if not os.path.exists(f'{path}/images'):
+            os.mkdir(f'{path}/images')
+        
+        if not os.path.exists(f'{path}/images/{prefix}'):
+            os.mkdir(f'{path}/images/{prefix}')
+        
+        x = data.meta.x[::,::skip]/1000
+        z = data.meta.z[::,::skip]/1000
+        index = int(data.data[0].t / data.meta.dt)
+        for inp in tqdm(data.data):
+            fig, ax = plt.subplots()
+            
+            if plotType == Plot.STREAM:
+                c = ax.pcolor(x, z, inp.b[::,::skip], cmap='plasma', shading='auto', zorder=0)#, norm=divnorm)
+                sp = ax.streamplot(x, z, inp.u[::,::skip], inp.w[::,::skip], color='k',   arrowsize=1, density=1, linewidth=0.5, zorder=1)#, linewidth=lw)#,    density=0.8) # color=lw, cmap='Greys')
+
+                fig.colorbar(c, ax=ax)
+
+            elif plotType == Plot.LINE:
+                if lineType == LineType.U or lineType == LineType.ALL:
+                    ax.plot(data.meta.x[0,::skip]/1000, inp.u[0][::skip], f'k{"--" if lineType == LineType.ALL else ""}')
+                if lineType == LineType.W or lineType == LineType.ALL:
+                    ax.plot(data.meta.x[0,::skip]/1000, inp.w[0][::skip]*10, f'k{"-" if lineType == LineType.ALL else ""}')
+                if lineType == LineType.B or lineType == LineType.ALL:
+                    ax.plot(data.meta.x[0,::skip]/1000, inp.b[0][::skip]*10, f'k{":" if lineType == LineType.ALL else ""}')
+                if lineType == LineType.P:
+                    ax.plot(data.meta.x[0,::skip]/1000, inp.p[0][::skip]*10, f'k')
+
+            if title: plt.title(data.meta.title(inp.t), pad=30)
+
+            fig.savefig(f'{path}/images/{prefix}/{index}.jpg', bbox_inches='tight', transparent=False, facecolor='white')
+            plt.close(fig)
+            
+            index += 1
+
+    def gif(self, input: str, output: str, framerate: int = 10):
+        file = output
+        if not output.__contains__('.gif'):
+            file = f'{output}/run.gif'
+        
+        subprocess.run([f'echo Y | ffmpeg -framerate {framerate} -start_number 0 -i {input}/%d.jpg -vf "scale=-2:512" -pix_fmt yuv420p {file}'], shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        
+    def display(self, data, time, prefix=''):
+        skip = 1#math.ceil(data.meta.x.shape[1]/200)
+        #print(f'SKIP: {math.ceil(data.meta.x.shape[1]/200)}')
+        inp = data.data[time]
+        fig, ax = plt.subplots()
+
+        #divnorm = colors.BoundaryNorm(np.linspace(-0.3, 0.3, 30), plt.get_cmap('bwr').N)
+        divnorm = colors.TwoSlopeNorm(vmin=-0.3, vcenter=0, vmax=0.3)
+        #divnorm=colors.TwoSlopeNorm(vcenter=0)
+
+        #c = ax.pcolor(data.meta.x[::,::skip]/1000, data.meta.z[::,::skip]/1000, inp.b[::,::skip] * (273 / 10), cmap=plt.get_cmap('bwr', 30), shading='auto', zorder=0, norm=divnorm)
+        c = ax.pcolor(data.meta.x/1000, data.meta.z/1000, inp.b * (273 / 10), cmap=plt.get_cmap('bwr', 30), zorder=0, norm=divnorm)
+
+        #sp = ax.streamplot(data.meta.x[::,::skip]/1000, data.meta.z[::,::skip]/1000, inp.u[::,::skip], inp.w[::,::skip], color='k',   arrowsize=1, density=1, linewidth=0.5, zorder=1)#, linewidth=lw)#,    density=0.8) # color=lw, cmap='Greys')
+        
+        
+        # was unsing sp = ax.streamplot(data.meta.x/1000, data.meta.z/1000, inp.u, inp.w, color='k',   arrowsize=1, density=1, linewidth=0.5, zorder=1)#, linewidth=lw)#,    density=0.8) # color=lw, cmap='Greys')
+        
+        
+        
+        fig.colorbar(c, ax=ax)
+        #plt.title(f'{prefix}{data.meta.title(inp.t)}', pad=30)
+        plt.show()
+
+    def display_line(self, data, time, lineType = LineType.ALL, prefix=''):
+        skip = math.ceil(data.meta.x.shape[1]/200)
+        inp = data.data[time]
+        fig, ax = plt.subplots()
+
+        if lineType == LineType.U or lineType == LineType.ALL:
+            ax.plot(data.meta.x[0,::skip]/1000, inp.u[0][::skip], f'b{"--" if lineType == LineType.ALL else ""}')
+        if lineType == LineType.V or lineType == LineType.ALL:
+            ax.plot(data.meta.x[0,::skip]/1000, inp.v[0][::skip], f'p{"--" if lineType == LineType.ALL else ""}')
+        if lineType == LineType.W or lineType == LineType.ALL:
+            ax.plot(data.meta.x[0,::skip]/1000, inp.w[0][::skip]*10, f'r{"-" if lineType == LineType.ALL else ""}')
+        if lineType == LineType.B or lineType == LineType.ALL:
+            ax.plot(data.meta.x[0,::skip]/1000, inp.b[0][::skip]*10, f'k{":" if lineType == LineType.ALL else ""}')
+        if lineType == LineType.P or lineType == LineType.ALL:
+            ax.plot(data.meta.x[0,::skip]/1000, inp.p[0][::skip]*10, f'g:')
+        plt.title(f'{prefix} at t = {inp.t}')
+        plt.show()
---
+++ src/data.py
@@ -0,0 +1,222 @@
+from cmath import pi
+import numpy as np
+import pickle
+from dataclasses import dataclass
+import lzma
+import json
+from math import ceil
+
+@dataclass
+class DataSample:
+    b:   np.ndarray
+    u:   np.ndarray
+    w:   np.ndarray
+    p:   np.ndarray
+    v:   np.ndarray
+    rho: np.ndarray
+    t:   float = 0
+
+    def __add__(self, other):
+        if self.t != other.t:
+            return None
+        else:
+            Tb = self.b + other.b
+            Tu = self.u + other.u
+            Tw = self.w + other.w
+            Tv = self.v + other.v
+            Tp = self.p + other.p
+            Trho = self.rho + other.rho
+            return DataSample(u=Tu, v=Tv, w=Tw, b=Tb, p=Tp, rho=Trho, t=self.t)
+    
+    def _2D(self):
+        self.u = np.atleast_2d(self.u)
+        self.v = np.atleast_2d(self.v)
+        self.w = np.atleast_2d(self.w)
+        self.b = np.atleast_2d(self.b)
+        self.p = np.atleast_2d(self.p)
+        self.rho = np.atleast_2d(self.rho)
+    
+    def save(self, path):
+        with lzma.open(path, 'wb') as f:
+            pickle.dump(self, f)
+
+    def load(path):
+        with lzma.open(path, 'rb') as f:
+            d = pickle.load(f)
+            return d
+
+def serialize(o):
+    dictionary = dict(o.__dict__)
+    dictionary.pop('x', None)
+    dictionary.pop('z', None)
+    dictionary.pop('X', None)
+    dictionary.pop('Z', None)
+    dictionary.pop('D1', None)
+    dictionary.pop('D2', None)
+    dictionary.pop('A_u', None)
+    dictionary.pop('B_u', None)
+    dictionary.pop('Ainv_u', None)
+    dictionary.pop('A_uNEW', None)
+    dictionary.pop('B_uNEW', None)
+    dictionary.pop('Ainv_uNEW', None)
+    
+    return dictionary
+
+@dataclass
+class Meta:
+    Qs:         np.ndarray
+    js:         np.ndarray
+    Ts:         np.ndarray = None
+ 
+    time:       int = 15500
+    dt:         float = 10
+ 
+    width:      int = 100
+    D:          int = 10000
+    L:          int = 50000
+ 
+    N:          float = 0.01
+     
+    f:          int = 0
+    n:          int = 0
+    alpha:      float = 0
+ 
+    x:          np.ndarray = None
+    z:          np.ndarray = None
+
+    maxGB:      int = 16 # 16GB
+    saveEvery:  int = 0
+
+    S0:         float = 3.6e-5
+
+    def __post_init__(self):
+        print(f'Generating metadata:')
+        print(f'Setting up space...')
+        self.space = self.width * self.L
+        self.dx = 100
+        self.dz = 50
+        #self.spacesteps = int(self.space / self.dx)
+        self.spacesteps = 1000
+        self.dx = int(self.space / self.spacesteps)
+        self.dz = int(self.D / self.spacesteps)
+
+        self.timesteps = ceil(self.time / self.dt)
+
+        self.X = np.linspace(-self.space / 2, self.space / 2, self.spacesteps, endpoint=False, dtype='float32')
+        #self.X = np.linspace(0, self.space, self.spacesteps, endpoint=False, dtype='float64')
+        self.Z = np.linspace(0, self.D, self.spacesteps, endpoint=True, dtype='float64')
+        self.x, self.z = np.meshgrid(self.X, self.Z)
+
+        if len(self.Qs) != len(self.js):
+            self.Qs = np.repeat(self.Qs, len(self.js))
+            self.Ts = np.repeat(self.Ts, len(self.js))
+
+        self.generateMatrices()
+
+    def generateMatrices(self):
+        # make differentiation matrices D1 and D2
+        print(f'Creating matrices...')
+        #self.D1 = np.zeros((self.spacesteps, self.spacesteps), dtype='float32')
+        #self.D2 = np.zeros((self.spacesteps, self.spacesteps), dtype='float32')
+
+        #self.D1[0,  1] =  1
+        #self.D1[0, -1] = -1
+        #self.D2[0,  0] = -2
+        #self.D2[0,  1] =  1
+        #self.D2[0, -1] =  1
+
+        #cx=1
+        #while cx < self.spacesteps - 1:
+        #    self.D1[cx, cx-1] = -1
+        #    self.D1[cx, cx+1] =  1
+        #    self.D2[cx, cx-1] =  1
+        #    self.D2[cx, cx]   = -2
+        #    self.D2[cx, cx+1] =  1
+        #    cx = cx + 1
+
+        #self.D1[self.spacesteps-1, self.spacesteps-2] = -1
+        #self.D1[self.spacesteps-1, 0]                 =  1
+        #self.D2[self.spacesteps-1, self.spacesteps-2] =  1
+        #self.D2[self.spacesteps-1, self.spacesteps-1] = -2
+        #self.D2[self.spacesteps-1, 0]                 =  1
+
+        spacesteps = self.spacesteps
+        D1 = np.zeros((spacesteps, spacesteps), dtype='float64')
+        D2 = np.zeros((spacesteps, spacesteps), dtype='float64')
+        eye = np.zeros((spacesteps, spacesteps), dtype='float64')
+        cx=0
+        while cx < spacesteps:
+            D1[cx, (cx+1) % spacesteps] = 1
+            D1[cx, (cx-1) % spacesteps] = -1
+
+            D2[cx, (cx-1) % spacesteps] =  1
+            D2[cx, (cx) % spacesteps]   = -2
+            D2[cx, (cx+1) % spacesteps] =  1
+
+            cx = cx + 1
+
+        eye = np.eye(spacesteps)
+
+
+        self.D1 = D1 / (2*self.dx)
+        #self.D1 = D1 / (self.dx)
+        #self.D2 = D2 / (self.dx**2)
+        self.D2 = D2 / (self.dx**2)
+
+        self.D1 = self.D1.astype('float64')
+        self.D2 = self.D2.astype('float64')
+
+        #self.A_u = np.array([np.eye(self.spacesteps, dtype='float32') - (((self.N ** 2) * (self.dt ** 2) / (4 * ((j * np.pi / self.D) ** 2))) * self.D2) for j in self.js])
+        #self.B_u = np.array([np.eye(self.spacesteps, dtype='float32') + (((self.N ** 2) * (self.dt ** 2) / (4 * ((j * np.pi / self.D) ** 2))) * self.D2) for j in self.js])
+        #self.Ainv_u = np.array([np.linalg.inv(A) for A in self.A_u], dtype='float32')
+
+        #Aparam = np.array([((self.dt**2)*(self.f**2)/(4)) - ((self.n/((j * np.pi / self.D)**2))*self.D2) - (((self.N ** 2) * (self.dt ** 2) / (4 * ((j * np.pi / self.D) ** 2) * (1 + (self.alpha * self.dt / 2)))) * self.D2) for j in self.js])
+        #self.A_uNEW = np.array([np.eye(self.spacesteps, dtype='float32') + A for A in Aparam], dtype='float32')
+        #self.B_uNEW = np.array([np.eye(self.spacesteps, dtype='float32') - A for A in Aparam], dtype='float32')
+        #self.Ainv_uNEW = np.array([np.linalg.inv(A) for A in self.A_uNEW])
+
+
+        h = 100000 # m - scale height
+        A_bulk = np.array([(((self.f**2) * (self.dt ** 2) / 4) - ((((self.N * self.D) ** 2) / (((j * pi) ** 2) + ((self.D ** 2)/(4 * (h ** 2))))) * (self.dt ** 2) * self.D2 / 4)) for j in self.js])
+        self.A_new = np.array([eye + A for A in A_bulk])
+        self.B_new = np.array([eye - A for A in A_bulk])
+        self.Ainv_new = np.array([np.linalg.inv(A) for A in self.A_new], dtype='float64')
+
+
+
+    def __str__(self):
+        extra = ""
+        if self.f != 0 or self.n != 0 or self.alpha != 0:
+            extra = f'_f{self.f}_n{self.n}_alpha{self.alpha}'
+        return f'j{"-".join(map(str, self.js))}_D{self.D}_L{self.L}_Q{("_Q_".join(map(str,self.Qs))).replace(".","-")}_N{str(self.N).replace(".","-")}_T{("_T_".join(map(str, self.Ts))).replace(".","-")}_T{self.time}_dt{str(self.dt).replace(".","-")}{extra}'
+
+    def title(self, t):
+        return f'Plot for t={int(t/60)}mins, \n N={self.N}, depth={self.D/1000}km, width={self.width}*{self.L/1000}km, \n modes=[{", ".join  (map(str, self.js))}], Qs=[{(", ".join(map(str,self.Qs)))}]'
+    
+    def json(self, path):        
+        with open(f'{path}/meta.json', 'w', encoding='utf-8') as f:
+            json.dump(serialize(self), f, default=lambda o: o.tolist(), indent=4)
+
+    def load(path):
+        meta = Meta()
+        with open(f'{path}', 'w', encoding='utf-8') as f:
+            meta.__dict__ = json.loads(f)
+        return meta
+
+@dataclass
+class Data:
+    data: np.ndarray
+    meta: Meta
+
+    def addSample(self, sample: DataSample):
+        self.data = np.append(self.data, [sample], 0)
+
+    def save(self, path, saveMeta:bool = False):
+        dataToSave = Data(meta=self.meta if saveMeta else None, data=self.data)
+        with lzma.open(f'{path}/{"META" if saveMeta else ""}data.pickle.xz', 'wb') as f:
+            pickle.dump(dataToSave, f)
+
+    def load(path):
+        with lzma.open(path, 'rb') as f:
+            d = pickle.load(f)
+            return d
