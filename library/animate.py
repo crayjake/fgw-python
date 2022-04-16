c5f51bffa19e4fdc4b5ae1e1b4f324813baafae8
crayjake <mm18j2c@leeds.ac.uk>
2022-04-16 11:53:34 +0100

    removed orientation of colourbar as mutually exclusive with location

--- library/animate.py
+++ library/animate.py
@@ -330,7 +330,7 @@ def plotGroup(leftData:        np.array,
         rax.set_ylim([0, np.max(meta.Z)])
 
     # axes.ravel().tolist()
-    cbar = fig.colorbar(c, ax=axes.ravel().tolist(), ticks=ticks, location='top', orientation='horizontal')
+    cbar = fig.colorbar(c, ax=axes.ravel().tolist(), ticks=ticks, location='top')
 
     fig.tight_layout()
     plt.savefig(f'data/groups/{directory}.jpg', bbox_inches = 'tight', pad_inches = 0)
